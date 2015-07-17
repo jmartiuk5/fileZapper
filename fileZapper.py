a4e93ce979b3d9f1553607644e6fb05cc1bd6d1a
James Martiuk <jmartiuk5@gmail.com>
2015-07-17 08:06:35 -0400

    my first real project

---
+++ fileZapper.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import re
+                    #DONT GIVE UP!!!
+match = compile('*.py*', traverse)
+traverse = os.walk('/Users/', topdown=True)
+#find a module to go through my files
+for root, dirs, files in traverse:                       # os.walk('/Users/', topdown=True):
+    if match:
+        print match.group()
+    for name in files:
+        print os.path.join(root, name)
+    for name in dirs:
+        print os.path.join(root, name)
+
+
+
+#find another module to check the extensions on the files
+
+
+#Write code that removes all files with supplied extension
+
+
+#Last but not least refine and remaster program
+
+
